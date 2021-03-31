df5b40ca3bfa5dc3fc606337a6c5fdad512068d9
SettingDust <settingdust@gmail.com>
2021-03-31 18:37:16 +0800

    :tada: Init

---
+++ src/main/java/com/lalameow/happycultivation/HappyCultivation.java
@@ -0,0 +1,10 @@
+package com.lalameow.happycultivation;
+
+import net.fabricmc.api.ModInitializer;
+
+public class HappyCultivation implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/lalameow/happycultivation/client/HappyCultivationClient.java
@@ -0,0 +1,13 @@
+package com.lalameow.happycultivation.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class HappyCultivationClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
